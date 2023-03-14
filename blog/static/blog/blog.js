dd099a4787020bc411e3956a398e14f549e9e3a0
sy-uname <sy.uname@gmail.com>
2023-03-14 15:58:53 +0400

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,14 +1,49 @@
-const One = 1;
-const theNumber = One;
-let yourName = 'Ben';
+for(let i = 0; i < 10; i += 1) {
+  console.log('for loop i: ' + i)
+}
 
-function print_alert(name, count){
-  alert(name + '[' + count + ']')
+let j = 0
+while(j < 10) {
+  console.log('while loop j: ' + j)
+  j += 1
+}
+
+let k = 10
+
+do {
+  console.log('do while k: ' + k)
+} while(k < 10)
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numbers.forEach((value => {
+  console.log('For each value ' + value)
+}))
+
+const doubled = numbers.map(value => value * 2)
+
+console.log('Here are the doubled numbers')
+
+console.log(doubled)
+
+function sayHello(yourName) {
+  if (yourName === undefined) {
+    console.log('Hello, no name')
+  } else {
+    console.log('Hello, ' + yourName)
+  };
+  console.timeEnd('myTimer');
 };
 
-if (theNumber === One) {
-  yourName = 'Leo';
-  print_alert(yourName + '!', theNumber);
-};
+console.time('myTimer')
+const yourName = 'Your Name'  // Put your name here
 
-print_alert(yourName, theNumber);
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(yourName)
+  }, 2000
+)
+
+console.log('After setTimeout')
+
